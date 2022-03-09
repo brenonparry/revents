f34c6a874bdfca39de1e8aef8dc61906617cb718
Brenon Parry <brenonparry@gmail.com>
2022-03-08 17:24:58 -0800

    updated folder structure and added NavBar styling

---
+++ src/app/layout/App.jsx
@@ -0,0 +1,16 @@
+import { Container } from 'semantic-ui-react';
+import EventDashboard from '../../features/events/eventDashboard/EventDashboard';
+import NavBar from '../../features/nav/NavBar';
+
+export default function App() {
+  return (
+    <div>
+      <NavBar />
+      <Container className='main'>
+        <EventDashboard />
+      </Container>
+    </div>
+  );
+}
+
+
--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import App from './app/layout/App';
 import 'semantic-ui-css/semantic.min.css';
-import './index.css';
+import './app/layout/styles.css';
 
 const rootElement = document.getElementById('root');
 
