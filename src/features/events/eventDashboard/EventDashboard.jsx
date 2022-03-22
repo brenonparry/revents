a8f1d6549ba77465dd3eda754a39659533c6328d
Brenon Parry <brenonparry@gmail.com>
2022-03-22 16:03:32 -0700

    add package cuid for creating new events, and updated state 'values' to include all event parameters

--- src/features/events/eventDashboard/EventDashboard.jsx
+++ src/features/events/eventDashboard/EventDashboard.jsx
@@ -1,21 +1,29 @@
-import React, {useState} from 'react'
-import { Grid } from 'semantic-ui-react'
-import EventForm from '../eventForm/EventForm'
-import EventList from './EventList'
-import { sampleData } from '../../../app/api/sampleData'
+import React, { useState } from "react";
+import { Grid } from "semantic-ui-react";
+import EventForm from "../eventForm/EventForm";
+import EventList from "./EventList";
+import { sampleData } from "../../../app/api/sampleData";
 
-export default function EventDashboard({formOpen, setFormOpen}) {
+export default function EventDashboard({ formOpen, setFormOpen }) {
   const [events, setEvents] = useState(sampleData);
-  
+  const handleCreateEvent = (event) => {
+    setEvents([...events, event]);
+  };
+
   return (
     <Grid>
       <Grid.Column width={10}>
         <EventList events={events} />
       </Grid.Column>
       <Grid.Column width={6}>
-        {formOpen && 
-        <EventForm setFormOpen={setFormOpen} setEvents={setEvents}/> }
+        {formOpen && (
+          <EventForm
+            setFormOpen={setFormOpen}
+            setEvents={setEvents}
+            createEvent={handleCreateEvent}
+          />
+        )}
       </Grid.Column>
     </Grid>
-  )
+  );
 }
--- src/features/events/eventForm/EventForm.jsx
+++ src/features/events/eventForm/EventForm.jsx
@@ -1,7 +1,8 @@
+import cuid from 'cuid';
 import React, { useState } from "react";
 import { Button, Form, Header, Segment } from "semantic-ui-react";
 
-export default function EventForm({ setFormOpen, setEvents }) {
+export default function EventForm({ setFormOpen, setEvents, createEvent }) {
   const initialValues = {
     title: "",
     category: "",
@@ -13,7 +14,8 @@ export default function EventForm({ setFormOpen, setEvents }) {
 
   const [values, setValues] = useState(initialValues);
   const handleFormSubmit = () => {
-    console.log(values);
+    createEvent({...values, id: cuid(), hostedBy: 'Lloyd', attendees: [], hostPhotoURL: '/assets/user.png'})
+    setFormOpen(false)
   };
 
   const handleInputChange = (e) => {
