c90b5a13290855b12ba2eae4529c696c3979f570
Brenon Parry <brenonparry@gmail.com>
2022-03-22 15:52:40 -0700

    add state to manage form

--- src/features/events/eventForm/EventForm.jsx
+++ src/features/events/eventForm/EventForm.jsx
@@ -1,32 +1,92 @@
-import React from 'react'
-import { Button, Form, Header, Segment } from 'semantic-ui-react'
+import React, { useState } from "react";
+import { Button, Form, Header, Segment } from "semantic-ui-react";
 
-export default function EventForm({setFormOpen, setEvents}) {
+export default function EventForm({ setFormOpen, setEvents }) {
+  const initialValues = {
+    title: "",
+    category: "",
+    description: "",
+    city: "",
+    venue: "",
+    date: "",
+  };
+
+  const [values, setValues] = useState(initialValues);
+  const handleFormSubmit = () => {
+    console.log(values);
+  };
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setValues({ ...values, [name]: value });
+  };
+
   return (
     <Segment clearing>
-      <Header content='Create new event'/>
-        <Form>
-          <Form.Field>
-            <input type='text' placeholder='Event Title'/>
-          </Form.Field>
-          <Form.Field>
-            <input type='text' placeholder='Category'/>
-          </Form.Field>
-          <Form.Field>
-            <input type='text' placeholder='Description'/>
-          </Form.Field>
-          <Form.Field>
-            <input type='text' placeholder='City'/>
-          </Form.Field>
-          <Form.Field>
-            <input type='text' placeholder='Venue'/>
-          </Form.Field>
-          <Form.Field>
-            <input type='date' placeholder='Date'/>
-          </Form.Field>
-          <Button type='submit' floated='right' positive content='Submit'/>
-          <Button onClick={() => setFormOpen(false)} type='submit' floated='right' content='Cancel'/>
-        </Form>
+      <Header content="Create new event" />
+      <Form onSubmit={handleFormSubmit}>
+        <Form.Field>
+          <input
+            type="text"
+            placeholder="Event Title"
+            name="title"
+            title={values.title}
+            onChange={(e) => handleInputChange(e)}
+          />
+        </Form.Field>
+        <Form.Field>
+          <input
+            type="text"
+            placeholder="Category"
+            name="category"
+            title={values.category}
+            onChange={(e) => handleInputChange(e)}
+          />
+        </Form.Field>
+        <Form.Field>
+          <input
+            type="text"
+            placeholder="Description"
+            name="description"
+            title={values.description}
+            onChange={(e) => handleInputChange(e)}
+          />
+        </Form.Field>
+        <Form.Field>
+          <input
+            type="text"
+            placeholder="City"
+            name="city"
+            title={values.city}
+            onChange={(e) => handleInputChange(e)}
+          />
+        </Form.Field>
+        <Form.Field>
+          <input
+            type="text"
+            placeholder="Venue"
+            name="venue"
+            title={values.venue}
+            onChange={(e) => handleInputChange(e)}
+          />
+        </Form.Field>
+        <Form.Field>
+          <input
+            type="date"
+            placeholder="Date"
+            name="date"
+            title={values.date}
+            onChange={(e) => handleInputChange(e)}
+          />
+        </Form.Field>
+        <Button type="submit" floated="right" positive content="Submit" />
+        <Button
+          onClick={() => setFormOpen(false)}
+          type="submit"
+          floated="right"
+          content="Cancel"
+        />
+      </Form>
     </Segment>
-  )
+  );
 }
