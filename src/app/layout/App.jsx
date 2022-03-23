f12d8d6e6a4463b99f885f390b421ba18878db58
Brenon Parry <brenonparry@gmail.com>
2022-03-23 14:57:58 -0700

    add react-router-dom, and routes for event components

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -1,7 +1,11 @@
 import { useState } from "react";
 import { Container } from "semantic-ui-react";
 import EventDashboard from "../../features/events/eventDashboard/EventDashboard";
+import HomePage from "../../features/home/HomePage";
 import NavBar from "../../features/nav/NavBar";
+import EventForm from "../../features/events/eventForm/EventForm";
+import { Route } from "react-router-dom";
+import eventDetailedPage from "../../features/events/eventsDetailed/eventDetailedPage";
 
 export default function App() {
   const [formOpen, setFormOpen] = useState(false);
@@ -21,12 +25,10 @@ export default function App() {
     <>
       <NavBar setFormOpen={handleCreateFormOpen} />
       <Container className="main">
-        <EventDashboard
-          formOpen={formOpen}
-          setFormOpen={setFormOpen}
-          selectEvent={handleSelectEvent}
-          selectedEvent={selectedEvent}
-        />
+        <Route exact path='/' component={HomePage}/>
+        <Route exact path='/events' component={EventDashboard}/>
+        <Route path='/events/:id' component={eventDetailedPage}/>
+        <Route path='/createEvent' component={EventForm}/>
       </Container>
     </>
   );
--- src/index.jsx
+++ src/index.jsx
@@ -1,15 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './app/layout/App';
-import 'semantic-ui-css/semantic.min.css';
-import './app/layout/styles.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./app/layout/App";
+import "semantic-ui-css/semantic.min.css";
+import "./app/layout/styles.css";
+import { BrowserRouter } from "react-router-dom";
 
-const rootElement = document.getElementById('root');
+const rootElement = document.getElementById("root");
 
 // const render = () => {
 // }
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  rootElement
+);
 
 // if (module.hot) {
-//   module.hot.accept('./App.jsx', () => {setTimeout(render)}); 
+//   module.hot.accept('./App.jsx', () => {setTimeout(render)});
 // }
