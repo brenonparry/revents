4ae81cb79b6521bfb69be2ce89fc412b863f1363
Brenon Parry <brenonparry@gmail.com>
2022-03-30 12:43:50 -0700

    add faux login credentials

--- src/features/nav/NavBar.jsx
+++ src/features/nav/NavBar.jsx
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Button, Container, Menu } from 'semantic-ui-react';
 import { NavLink } from 'react-router-dom';
 import SignedOutMenu from './SignedOutMenu';
 import SignedInMenu from './SignedInMenu';
 
 export default function NavBar({setFormOpen}) {
+  const [authenticated, setAuthenticated] = useState(false)
   return (
     <Menu inverted fixed='top'>
       <Container>
@@ -13,11 +14,11 @@ export default function NavBar({setFormOpen}) {
           Re-Vents
         </Menu.Item>
         <Menu.Item as={NavLink} to='/events' name='Events'/>
+        {authenticated && 
         <Menu.Item as={NavLink} to='/createEvent'>
           <Button positive inverted content='Create Event'/>
-        </Menu.Item>
-        <SignedOutMenu/>
-        <SignedInMenu />
+        </Menu.Item>}
+        {authenticated ? <SignedInMenu setAuthenticated={setAuthenticated}/> : <SignedOutMenu setAuthenticated={setAuthenticated}/>}
       </Container>
     </Menu>
   )
