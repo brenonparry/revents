e355162656706615e8ee06db5a95de0b8c33b000
Brenon Parry <brenonparry@gmail.com>
2022-03-30 10:28:21 -0700

    add contetnt to the home page

--- src/features/home/HomePage.jsx
+++ src/features/home/HomePage.jsx
@@ -1,7 +1,19 @@
 import React from 'react'
+import { Container, Header, Segment, Image, Button, Icon } from 'semantic-ui-react'
 
 export default function HomePage() {
   return (
-    <h1>Home Page</h1>
+    <Segment inverted textAlign='center' vertical className='masthead'>
+      <Container>
+        <Header as='h1' inverted>
+          <Image size='massive' src='/assets/logo.png' style={{marginBottom: 12}}/>
+          Re-Vents
+        </Header>
+        <Button size='huge' inverted>
+          Get Started!
+          <Icon name='right arrow' inverted/>
+        </Button>
+      </Container>
+    </Segment>
   )
 }
