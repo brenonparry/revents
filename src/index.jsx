38f00588e5fed9d415b3df9096cb554358f76fca
Brenon Parry <brenonparry@gmail.com>
2022-03-31 12:07:02 -0700

    add react-redux and set up store, test area, and wrap app in provider

--- src/index.jsx
+++ src/index.jsx
@@ -4,15 +4,21 @@ import App from "./app/layout/App";
 import "semantic-ui-css/semantic.min.css";
 import "./app/layout/styles.css";
 import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { configureStore } from "./app/store/configureStore";
 
+const store = configureStore();
 const rootElement = document.getElementById("root");
 
 // const render = () => {
 // }
 ReactDOM.render(
+  <Provider store={store}>
   <BrowserRouter>
     <App />
-  </BrowserRouter>,
+  </BrowserRouter>
+  </Provider>
+  ,
   rootElement
 );
 
